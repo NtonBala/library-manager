85fb584954362adc250f00250fbea57f219ac945
Anton Balashov <balashov.anton.1983@gmail.com>
2023-08-26 11:36:46 +0300

    Refactor utilityFunctions.ts to use export statement

--- lib/utilityFunctions.ts
+++ lib/utilityFunctions.ts
@@ -1,8 +1,8 @@
-export function CalculateLateFee(daysLate: number): number {
+function CalculateLateFee(daysLate: number): number {
   return daysLate * 0.25;
 }
 
-export function MaxBooksAllowed(age: number): number {
+function MaxBooksAllowed(age: number): number {
   if (age < 12) {
     return 3;
   } else {
@@ -13,3 +13,5 @@ export function MaxBooksAllowed(age: number): number {
 function privateFunc(): void {
   console.log('This is private...');
 }
+
+export { CalculateLateFee, MaxBooksAllowed };
